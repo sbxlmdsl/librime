1945281fffa9a901da4e4d4a5080ad49d296485f
dsl <sbxlm@qq.com>
2020-01-01 08:46:26 +0800

    removed code for restricting word creation in sbjm

--- src/rime/algo/encoder.cc
+++ src/rime/algo/encoder.cc
@@ -279,12 +279,7 @@ bool TableEncoder::DfsEncode(const string& phrase,
 			  encoded.replace(2, 2, (*code)[1].substr(2, 2));
 		  }
 	  }
-	  else if (code->size() == 2 && dict_name_ == "sbjm") {
-		  if (boost::regex_match((*code)[0], boost::regex("^[qwrtsdfgzxcvbyphjklnm][aeiou]?$"))
-			  && boost::regex_match((*code)[1], boost::regex("^[qwrtsdfgzxcvbyphjklnm][aeiou]?$"))) {
-			  return false;
-		  }
-	  }
+
 	  if (dict_name_ == "sbjm") {
 		collector_->CreateEntry(encoded.substr(3) + " " + phrase, encoded.substr(0, 3), value);
 	  }
