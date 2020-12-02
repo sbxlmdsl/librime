e2c9fb0fc6a4ad3d3411d931c62e9ec45e494440
dsl <1020889210@qq.com>
2020-12-02 11:29:01 +0800

    support sbxh and sbzr

--- src/rime_api.cc
+++ src/rime_api.cc
@@ -358,6 +358,8 @@ RIME_API Bool RimeGetContext(RimeSessionId session_id, RimeContext* context) {
               std::strcpy(context->select_labels[i], labels[i].c_str());
 			else if (boost::regex_match(schema->schema_id(), boost::regex("^sbjz$")) && !ctx->IsSixth())
 				std::strcpy(context->select_labels[i], labels[i].c_str());
+			else if (boost::regex_match(schema->schema_id(), boost::regex("^sb[hz]z$")) && !ctx->IsFourth())
+				std::strcpy(context->select_labels[i], labels[i].c_str());
 			else
               std::strcpy(context->select_labels[i], label.c_str());
           }
