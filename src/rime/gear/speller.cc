059b03d3e0de96223b8963a74241e289363f0a16
DSL <sbxlm@qq.com>
2021-02-21 22:16:10 +0800

    remove more bugs in ejecting multi-char words for sb[fk]x

--- src/rime/gear/speller.cc
+++ src/rime/gear/speller.cc
@@ -207,7 +207,8 @@ bool Speller::AutoSelectPreviousMatch(Context* ctx,
   string converted = input.substr(0, end);
   auto cand = previous_segment->GetSelectedCandidate();
   if (5 == input.length() && is_table_entry(cand)
-    && !(string("aeuio").find(ctx->input()[1]) != string::npos && string("aeuio_").find(ctx->input()[2]) != string::npos)
+	&& string("QWRTSDFGZXCVBYPHJKLNM").find(ctx->input()[3]) == string::npos
+	&& !(string("aeuio").find(ctx->input()[1]) != string::npos && string("aeuio_").find(ctx->input()[2]) != string::npos)
     && !(string("aeuio").find(ctx->input()[1]) == string::npos && string("aeuio_").find(ctx->input()[2]) != string::npos)
     && boost::regex_match(engine_->schema()->schema_id(), boost::regex("^sb[fk]x$"))) {
       return FindEarlierMatch(ctx, start ,end - 1);
