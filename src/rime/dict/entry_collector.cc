a61e1f7890d00567bdfafdb6b776246af6034d5c
sbxlmdsl <sbxlmdsl@qq.com>
2023-11-15 12:14:37 +0800

    correct lint errors in entry_collector.cc

--- src/rime/dict/entry_collector.cc
+++ src/rime/dict/entry_collector.cc
@@ -89,8 +89,7 @@ void EntryCollector::Collect(const string& dict_file) {
     }
     // read a dict entry
     vector<string> row;
-    boost::algorithm::split(row, line,
-                            boost::algorithm::is_any_of("\t"));
+    boost::algorithm::split(row, line, boost::algorithm::is_any_of("\t"));
     int num_columns = static_cast<int>(row.size());
     if (num_columns <= text_column || row[text_column].empty()) {
       LOG(WARNING) << "Missing entry text at #" << num_entries << ".";
